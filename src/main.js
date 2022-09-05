e9998b35a18363c6e5b2c9af92b04922f4477828
BetaCatPro <zj20162325@163.com>
2022-09-05 21:13:30 +0800

    feat: 长列表通用组件及图片懒加载

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import './styles/index.css'
 import { useREM } from './utils/flexible'
 import useTheme from './utils/theme'
 import mLibs from './libs/index'
+import mDirectives from './directives/index'
 import 'virtual:svg-icons-register'
 
 useREM()
@@ -12,4 +13,5 @@ useTheme()
 const app = createApp(App)
 
 app.use(mLibs)
+app.use(mDirectives)
 app.mount('#app')
